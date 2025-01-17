7f81cd108447a82249b8613b67e39e14882783a5
vaibhavkedari07 <161722150+vaibhavkedari07@users.noreply.github.com>
2025-01-17 16:15:10 +0530

    Implement addition functionality to pass the test

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -21,7 +21,9 @@ const Calculator = () => {
     }
   };
 
-  const add = (a: number, b: number) => {};
+  const add = (a: number, b: number) => {
+    return a + b;
+  };
 
   const subtract = (a: number, b: number) => {};
 
