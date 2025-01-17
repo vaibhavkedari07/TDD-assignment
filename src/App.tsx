5dcc8355b8dcaa3a4a03df3be7e1979b952456ae
vaibhavkedari07 <161722150+vaibhavkedari07@users.noreply.github.com>
2025-01-17 15:32:54 +0530

    Implement calculator component to pass the test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Calculator from "./Calculator";
 
 function App() {
-  return <></>;
+  return <Calculator />;
 }
 
 export default App;
---
+++ src/Calculator.tsx
@@ -0,0 +1,5 @@
+const Calculator = () => {
+  return <div className="grid grid-cols-4" data-testid="calculator"></div>;
+};
+
+export default Calculator;
